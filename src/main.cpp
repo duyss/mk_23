511c87431a46c6ebcb64633925a58dd3699dda6a
Кулаев В <kulaev.v@yandex.ru>
2023-11-14 18:07:22 +0300

    Добавлено мигание

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
 int main () {
 
-    while (true) {
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
 
+    while (true) {
+        gpio_toggle(GPIOE, GPIO8 | GPIO12);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+        gpio_toggle(GPIOE, GPIO9 | GPIO13);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+        gpio_toggle(GPIOE, GPIO10 | GPIO14);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+        gpio_toggle(GPIOE, GPIO11 | GPIO15);
+        for(volatile uint32_t i = 0; i < 300'000; i++);
+
+         gpio_toggle(GPIOE, GPIO11 | GPIO15);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+         gpio_toggle(GPIOE, GPIO10 | GPIO14);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+         gpio_toggle(GPIOE, GPIO9 | GPIO13);
+        for(volatile uint32_t i = 0; i < 50'000; i++);
+         gpio_toggle(GPIOE, GPIO8 | GPIO12);
+        for(volatile uint32_t i = 0; i < 300'000; i++);
+        
+        
     }
 }
