63786d42a17822aaca1caa54530345694b3f6f88
Кулаев В <kulaev.v@yandex.ru>
2023-11-28 17:22:32 +0300

    Убраны старые ветовые эффекты, добавлено включение одного светодиода

--- src/main.cpp
+++ src/main.cpp
@@ -7,72 +7,10 @@ int main () {
     );
 
     rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
 
     while (true) {
-        gpio_toggle(GPIOE, GPIO8 | GPIO12);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO9 | GPIO13);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO10 | GPIO14);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO11 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-
-        gpio_toggle(GPIOE, GPIO11 | GPIO15);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO10 | GPIO14);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO9 | GPIO13);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO12);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-
         gpio_toggle(GPIOE, GPIO8);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO9);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO10);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO11);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO12);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO13);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO14);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-
-        gpio_toggle(GPIOE, GPIO15);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO14);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO13);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO12);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO11);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO10);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO9);
-        for(volatile uint32_t i = 0; i < 50'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 300'000*8; i++);
-        gpio_toggle(GPIOE, GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 | GPIO14 | GPIO15);
-        for(volatile uint32_t i = 0; i < 1000'000*8; i++);
+        
     }
 }
