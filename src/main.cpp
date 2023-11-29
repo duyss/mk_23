07a81a31542f0faab0e76c1d0500bd4bc66586d1
Кулаев В <kulaev.v@yandex.ru>
2023-11-29 16:27:35 +0300

    5.1 добавлен новый таймер

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 
 
 constexpr uint16_t PERIOD_MS(1000);
@@ -19,6 +20,10 @@ void timer_setup () {
     rcc_periph_clock_enable(RCC_TIM1);
     timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
     timer_set_period(TIM1, PERIOD_MS - 1);
+
+    timer_enable_irq(TIM1, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM1_UP_TIM16_IRQ);
+
     timer_enable_counter(TIM1);
 }
 
@@ -33,4 +38,9 @@ int main () {
     while (true) {
         blink_LED();
     }
+}
+
+void tim1_up_tim16_isr(void) {
+    timer_clear_flag(TIM1, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO11);
 }
