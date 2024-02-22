dfded438fcbf3e3961613a391c7aff4d7db43c16
Кулаев В <kulaev.v@yandex.ru>
2024-02-22 14:46:23 +0300

    lab 3

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,25 @@
 void setup () {
     rcc_periph_clock_enable(RCC_GPIOA);
 
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);
-    gpio_set_af(GPIOA,GPIO_AF7, GPIO9 | GPIO10);
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
+    gpio_set_af(GPIOA,GPIO_AF7, GPIO2 | GPIO3);
 
-    rcc_periph_clock_enable(RCC_USART1);
+    rcc_periph_clock_enable(RCC_USART2);
 
-    usart_set_baudrate(USART1, 115200);
-    usart_set_databits(USART1, 8);
-    usart_set_stopbits(USART1, USART_STOPBITS_1);
-    usart_set_parity(USART1, USART_PARITY_NONE);
+    usart_set_baudrate(USART2, 19200);
+    usart_set_databits(USART2, 8);
+    usart_set_stopbits(USART2, USART_STOPBITS_1);
+    usart_set_parity(USART2, USART_PARITY_NONE);
 
-    usart_set_mode(USART1, USART_MODE_TX_RX);
-    usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART2, USART_MODE_TX_RX);
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
 
-    usart_enable(USART1);
+    usart_enable(USART2);
 }
 
 void loop () {
-    uint8_t c = usart_recv_blocking(USART1);
-    usart_send_blocking(USART1, c);
+    uint16_t z = usart_recv_blocking(USART2);
+    usart_send_blocking(USART2, z);
 }
 
 int main () {
